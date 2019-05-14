63a5ddd142227aa3a1d8c56eaed7d863d572a34b
Jason Martakis <45948606+jason6331@users.noreply.github.com>
2019-05-14 13:42:45 +0300

    Update botaki.py

--- botaki.py
+++ botaki.py
@@ -19,7 +19,7 @@ async def help(ctx, *command):
     jason = discord.utils.get(guild.members, id=368359878651019265)
     if mod1 in ctx.author.roles or mod2 in ctx.author.roles or mod3 in ctx.author.roles or ctx.author == jason:
         if c == '':
-            embed=discord.Embed(title="Commands of HF Bot", description="`.help .dm`", color=0x1bb6f1)
+            embed=discord.Embed(title="Commands of HF Bot", description="`help dm`", color=0x1bb6f1)
             embed.add_field(name="Do `.help [commands...]` for more info about it.", value="Command from: " + str(ctx.message.channel), inline=True)
             embed.set_footer(text="Bot made by jason6331")
             await ctx.author.send(embed=embed)
